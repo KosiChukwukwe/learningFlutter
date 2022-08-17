3875732dece8d96c3a7071d36f6aa11ce7790804
KosiChukwukwe <deborahchukwukwe@yahoo.com>
2022-08-17 14:32:23 +0100

    new codes added from line 40 - 70

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, camel_case_types, library_private_types_in_public_api, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: use_key_in_widget_constructors, camel_case_types, library_private_types_in_public_api, prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 
@@ -14,6 +14,21 @@ class learningFlutter extends StatefulWidget {
 }
 
 class _State extends State<learningFlutter> {
+  String Texts = "Hi there, welcome to my app";
+  var value = "Click to know the date and time";
+
+  void onClick() {
+    setState(() {
+      Texts = "My name is Kosi.";
+    });
+  }
+
+  void onPressed() {
+    setState(() {
+      value = DateTime.now().toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +41,30 @@ class _State extends State<learningFlutter> {
         child: Center(
           child: Column(
             children: <Widget>[
-              Text('hello world'),
+              Text(Texts),
+              ElevatedButton(
+                  onPressed: onClick,
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                  ),
+                  child: Text(
+                    'Click me',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Text(value),
+              TextButton(
+                  onPressed: onPressed,
+                  child: Text(
+                    'Click me',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))
             ],
           ),
         ),
