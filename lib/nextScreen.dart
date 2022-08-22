1803e3c312bfb3b7526f0db4480139262cc8d650
KosiChukwukwe <deborahchukwukwe@yahoo.com>
2022-08-22 13:39:07 +0100

    added a new screen titled more widgets

--- lib/nextScreen.dart
+++ lib/nextScreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -10,10 +10,62 @@ class NextScreen extends StatefulWidget {
 }
 
 class _NextScreenState extends State<NextScreen> {
+  void buttomPopUp() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(15),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              'Enjoying The App?',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            IconButton(
+              onPressed: (() => Navigator.pop(context)),
+              icon: Icon(Icons.cancel),
+            )
+          ]),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-    );
+        appBar: AppBar(
+          title: Text('more widgets'),
+        ),
+        body: Center(
+          child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'BOTTOM SHEET WIDGET',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  ElevatedButton(
+                      onPressed: buttomPopUp,
+                      child: Text('Click me & look below')),
+                  Divider(
+                    thickness: 2,
+                  ),
+                ],
+              )
+
+              // how to use sliders, remember to check above
+              // for the functions needed...
+
+              ),
+        ));
   }
 }
